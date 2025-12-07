4e011deea6ad178183281c3d378b14157d621f5d
Pathri Vidya Praveen <vidyapraveen.2006@gmail.com>
2025-12-07 17:09:04 +0530

    Add files via upload

---
+++ Introductory Problems/missing_number.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    int n;
+    cin >> n;
+    int a[n-1];
+    bool b[n];
+    for(int i=0;i<n;i++){
+        b[i] = false;
+    }
+    for(int i=0;i<n-1;i++){
+        cin >> a[i];
+        b[a[i]-1] = true;
+    }
+
+    for(int i=0;i<n;i++){
+        if(b[i]  == false){
+            cout << i+1 << endl;
+            break;
+        }
+    }
+
+
+    return 0;
+}
